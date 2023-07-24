1d73eec2822be75eacb3114ef9d4f0a43329234e
farisi18 <jaka18fa@gmail.com>
2023-07-24 13:41:40 +0700

    first commit

---
+++ app/src/androidTest/java/com/azhar/banksampah/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package com.azhar.banksampah
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.farisi.banksampah", appContext.packageName)
+    }
+}
---
+++ app/src/test/java/com/azhar/banksampah/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.azhar.banksampah
+
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, (2 + 2).toLong())
+    }
+}
